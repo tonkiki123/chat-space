0b148bc693c2644a106dc89c9bd4bc51a90bef34
tonkiki123 <67257028+tonkiki123@users.noreply.github.com>
2020-07-06 11:21:16 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -49,7 +49,6 @@ ActiveRecord::Schema.define(version: 2020_06_30_050606) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
